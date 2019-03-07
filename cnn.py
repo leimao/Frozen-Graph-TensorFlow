255fccc0cccea1218daf33ac3d4e7e5414a08ced
Lei Mao <dukeleimao@gmail.com>
2019-03-07 12:06:28 -0800

    update

--- cnn.py
+++ cnn.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import os
 
 from tensorflow.python.tools import freeze_graph
+from tensorflow.python.framework import graph_util
 
 class CNN(object):
 
@@ -191,8 +192,22 @@ class CNN(object):
         # This will only save the graph but the variables will not be saved.
         # You have to freeze your model first.
         tf.train.write_graph(graph_or_graph_def=self.sess.graph_def, logdir=directory, name=pbtxt_filename, as_text=True)
+
         # Freeze graph
-        freeze_graph.freeze_graph(input_graph=pbtxt_filepath, input_saver="", input_binary=False, input_checkpoint=ckpt_filepath, output_node_names="cnn/output", restore_op_name="save/restore_all", filename_tensor_name="save/Const:0", output_graph=pb_filepath, clear_devices=True, initializer_nodes="")
+        # Method 1
+        freeze_graph.freeze_graph(input_graph=pbtxt_filepath, input_saver='', input_binary=False, input_checkpoint=ckpt_filepath, output_node_names='cnn/output', restore_op_name='save/restore_all', filename_tensor_name='save/Const:0', output_graph=pb_filepath, clear_devices=True, initializer_nodes='')
+        
+        # Method 2
+        '''
+        graph = tf.get_default_graph()
+        input_graph_def = graph.as_graph_def()
+        output_node_names = ['cnn/output']
+
+        output_graph_def = graph_util.convert_variables_to_constants(self.sess, input_graph_def, output_node_names)
+
+        with tf.gfile.GFile(pb_filepath, 'wb') as f:
+            f.write(output_graph_def.SerializeToString())
+        '''
         
         return pb_filepath
 
